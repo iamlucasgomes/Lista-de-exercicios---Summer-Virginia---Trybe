8bfb6a580f2c7bfe5a4131ee878acd66d9e3a863
Lucas Gomes <lucas.devjs@gmail.com>
2022-04-07 14:11:51 -0300

    Add files via upload

---
+++ Exercicio - 03.js
@@ -0,0 +1,9 @@
+//  3. Escreva uma função que receba como parâmetros o nome, o sobrenome, a idade e a cidade de uma pessoa, e que retorne uma concatenação com os dados fornecidos.
+ 
+function  dados(nome, sobrenome,  idade,  cidade){ // Aqui criei uma função com o nome dados para receber os valores que queremos retornar com a função
+
+  var resultado = nome  + " " + sobrenome + ", "  + idade + ", "  + cidade  ; // aqui criei a variável resultado e atribui os dados concatenados
+    return resultado; // aqui usei o comando return para retornar a variável resultado quando usarmos a função
+}
+
+
