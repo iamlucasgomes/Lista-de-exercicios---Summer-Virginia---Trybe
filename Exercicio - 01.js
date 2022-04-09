12a3fc52efefa96e3ee7e06a3dfb86c992d986e6
Lucas Gomes <lucas.devjs@gmail.com>
2022-04-09 10:51:08 -0300

    Add files via upload

--- Exercicio - 01.js
+++ Exercicio - 01.js
@@ -5,13 +5,14 @@
  
 function nota(nota1,  nota2){
 
-    var resultado;//	variavel onde vou atribuir o resultado 
-    
-    if((nota1 + nota2)  / 2 >=  7){ //	se	a soma da nota1	com a nota2 dividido por 2 for maior ou igual a 7
-      resultado = "Aprovado";   //	"Aprovado" é atribuido a variável resultado  
-    } else if((nota1  + nota2)  / 2 < 7){   //	se	a soma da nota1	com a nota2 dividido por 2 for menor que 7
-      resultado = "Reprovado";  //	"Reprovado" é atribuido a variável resultado 
-    } if((nota1 + nota2)  / 2 ==  10){  //	se	a soma da nota1	com a nota2 dividido por 2 for igual a 10
-      resultado = "Aprovado com Distinção"; //	"Aprovado com Distinção" é atribuido a variável resultado 
-    } return resultado; //usamos o return para retornar o valor a variável resultado
-  }
+  var resultado;	//	variavel onde vou atribuir o resultado 
+  var media = (nota1 + nota2) / 2;	//	aqui criei uma variável com o nome média onde atribuimos a formula para encontrar uma média
+
+  if(media >=  7){ //	se	a média for maior ou igual a 7
+    resultado = "Aprovado";   //	"Aprovado" é atribuido a variável resultado  
+  } else if(media < 7){   //	se	a média for menor que 7
+    resultado = "Reprovado";  //	"Reprovado" é atribuido a variável resultado 
+  } if(media ===  10){  //	se	a média for igual a 10
+    resultado = "Aprovado com Distinção"; //	"Aprovado com Distinção" é atribuido a variável resultado 
+  } return resultado; //usamos o return para retornar o valor a variável resultado
+}
