36a99c09cc0d6adbe30258b9c8d04b6c7b0d2b26
Lucas Gomes <lucas.devjs@gmail.com>
2022-04-24 12:36:50 -0300

    Add files via upload

--- Exercicio - 01.js
+++ Exercicio - 01.js
@@ -2,17 +2,17 @@
 //- A mensagem "Aprovado", se a média alcançada for maior ou igual a sete;
 //- A mensagem "Reprovado", se a média for menor do que sete;
 //- A mensagem "Aprovado com Distinção", se a média for igual a dez.
- 
-function nota(nota1,  nota2){
+
+function nota(nota1, nota2) {
 
   var resultado;	//	variavel onde vou atribuir o resultado 
   var media = (nota1 + nota2) / 2;	//	aqui criei uma variável com o nome média onde atribuimos a formula para encontrar uma média
 
-  if(media >=  7){ //	se	a média for maior ou igual a 7
+  if (media >= 7) { //	se	a média for maior ou igual a 7
     resultado = "Aprovado";   //	"Aprovado" é atribuido a variável resultado  
-  } else if(media < 7){   //	se	a média for menor que 7
+  } else {   //	senão for a opção anterior
     resultado = "Reprovado";  //	"Reprovado" é atribuido a variável resultado 
-  } if(media ===  10){  //	se	a média for igual a 10
+  } if (media === 10) {  //	se	a média for igual a 10
     resultado = "Aprovado com Distinção"; //	"Aprovado com Distinção" é atribuido a variável resultado 
   } return resultado; //usamos o return para retornar o valor a variável resultado
 }
